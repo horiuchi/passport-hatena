0daa4f5905a0dbf685a65a316afbeb826c00e566
syon <syon.xiii@gmail.com>
2018-11-25 23:34:27 +0900

    refactoring

--- examples/express3/app.js
+++ examples/express3/app.js
@@ -1,11 +1,10 @@
-var express = require('express')
-  , passport = require('passport')
-  , util = require('util')
-  , HatenaStrategy = require('passport-hatena').Strategy;
+var express = require('express');
+var passport = require('passport');
+var HatenaStrategy = require('passport-hatena').Strategy;
 
-var HATENA_CONSUMER_KEY = "--insert-hatena-consumer-key-here--"
-var HATENA_SECRET_KEY = "--insert-hatena-secret-key-here--";
-
+var HATENA_CONSUMER_KEY = '--insert-hatena-consumer-key-here--';
+var HATENA_SECRET_KEY = '--insert-hatena-secret-key-here--';
+var CALLBACK_URL = 'http://localhost:3000/auth/hatena/callback';
 
 // Passport session setup.
 //   To support persistent login sessions, Passport needs to be able to
@@ -22,31 +21,29 @@ passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
 
-
 // Use the HatenaStrategy within Passport.
 //   Strategies in passport require a `verify` function, which accept
 //   credentials (in this case, a token, tokenSecret, and Hatena profile), and
 //   invoke a callback with a user object.
-passport.use(new HatenaStrategy({
-    consumerKey: HATENA_CONSUMER_KEY,
-    consumerSecret: HATENA_SECRET_KEY,
-    callbackURL: "http://127.0.0.1:3000/auth/hatena/callback"
-  },
-  function(token, tokenSecret, profile, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-      
-      // To keep the example simple, the user's Hatena profile is returned to
-      // represent the logged-in user.  In a typical application, you would want
-      // to associate the Hatena account with a user record in your database,
-      // and return that user instead.
-      return done(null, profile);
-    });
-  }
-));
-
-
-
+passport.use(
+  new HatenaStrategy(
+    {
+      consumerKey: HATENA_CONSUMER_KEY,
+      consumerSecret: HATENA_SECRET_KEY,
+      callbackURL: CALLBACK_URL
+    },
+    function(token, tokenSecret, profile, done) {
+      // asynchronous verification, for effect...
+      process.nextTick(function() {
+        // To keep the example simple, the user's Hatena profile is returned to
+        // represent the logged-in user.  In a typical application, you would want
+        // to associate the Hatena account with a user record in your database,
+        // and return that user instead.
+        return done(null, profile);
+      });
+    }
+  )
+);
 
 var app = express();
 
@@ -67,16 +64,15 @@ app.configure(function() {
   app.use(express.static(__dirname + '/public'));
 });
 
-
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.render('index', { user: req.user });
 });
 
-app.get('/account', ensureAuthenticated, function(req, res){
+app.get('/account', ensureAuthenticated, function(req, res) {
   res.render('account', { user: req.user });
 });
 
-app.get('/login', function(req, res){
+app.get('/login', function(req, res) {
   res.render('login', { user: req.user });
 });
 
@@ -85,31 +81,34 @@ app.get('/login', function(req, res){
 //   request.  The first step in Hatena authentication will involve redirecting
 //   the user to hatena.ne.jp.  After authorization, Hatena will redirect the user
 //   back to this application at /auth/hatena/callback
-app.get('/auth/hatena',
+app.get(
+  '/auth/hatena',
   passport.authenticate('hatena', { scope: ['read_public', 'write_public'] }),
-  function(req, res){
+  function(req, res) {
     // The request will be redirected to Hatena for authentication, so this
     // function will not be called.
-  });
+  }
+);
 
 // GET /auth/hatena/callback
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
-app.get('/auth/hatena/callback', 
+app.get(
+  '/auth/hatena/callback',
   passport.authenticate('hatena', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
-  });
+  }
+);
 
-app.get('/logout', function(req, res){
+app.get('/logout', function(req, res) {
   req.logout();
   res.redirect('/');
 });
 
 app.listen(3000);
-
 
 // Simple route middleware to ensure user is authenticated.
 //   Use this route middleware on any resource that needs to be protected.  If
@@ -117,6 +116,8 @@ app.listen(3000);
 //   the request will proceed.  Otherwise, the user will be redirected to the
 //   login page.
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
 }
